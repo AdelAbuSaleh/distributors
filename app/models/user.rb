a133d6eaf2fc5cb691c0ed59bac15feeffc15ab4
Adel Saleh <adel_saleh@clickapps.co>
2019-08-28 16:48:36 +0300

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,11 +18,24 @@
 class User < ApplicationRecord
   ## -------------------- Requirements -------------------- ##
   has_secure_password
+  extend Enumerize
   ## ----------------------- Scopes ----------------------- ##
   ## --------------------- Constants ---------------------- ##
   ## ----------------------- Enums ------------------------ ##
+  enumerize :role, in: {
+    admin: 0,
+    employee: 1
+  }, default: 1, scope: true, predicates: true
+
+  # User statuses
+  enumerize :status, in: {
+    active: 0,
+    inactive: 1
+  }, default: 1, scope: true, predicates: true
   ## -------------------- Associations -------------------- ##
   ## -------------------- Validations --------------------- ##
+  validates :role, presence: true
+  validates :status, presence: true
   ## --------------------- Callbacks ---------------------- ##
   ## ------------------- Class Methods -------------------- ##
   ## ---------------------- Methods ----------------------- ##

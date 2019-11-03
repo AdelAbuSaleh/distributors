90ebf47b9f348686f8e50ca81ad56e7b4404751c
Adel Saleh <adel_saleh@clickapps.co>
2019-11-03 12:18:59 +0300

    Destroy unused model

--- db/migrate/20191031074317_create_v1_users.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class CreateV1Users < ActiveRecord::Migration[5.2]
-  def change
-    create_table :v1_users, &:timestamps
-  end
-end
--- spec/factories/v1/users.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :v1_user, class: 'V1::User' do
-  end
-end
